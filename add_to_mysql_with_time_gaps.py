2a605c3fb58c8954084895e409d116dc380f6ef3
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-04-20 23:28:39 +0530

    Update implementation of adding a batch of data

--- add_to_mysql_with_time_gaps.py
+++ add_to_mysql_with_time_gaps.py
@@ -5,7 +5,7 @@ import time
 
 # Establish a connection to the MySQL database
 mydb = mysql.connector.connect(
-  host="172.17.0.4",
+  host="10.8.100.246",
   user="root",
   password="debezium",
   database="inventory"
@@ -17,23 +17,18 @@ cursor = mydb.cursor()
 # Define the SQL query to insert data into the table
 sql = "INSERT INTO networkTraffic (ip, date, timestamp, browser, traffic, eventTimestamp) VALUES (%s, %s, %s, %s, %s, %s)"
 
-# Run the loop 100 times
-for i in range(100):
+n=1
+# Run the loop n times
+for i in range(1):
     # Read data from the CSV file
     with open('records.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader) # Skip the header row
-        data = [(row[0], row[1], row[2], row[-1],int(row[-7]),datetime.utcnow().replace(tzinfo=timezone.utc)) for row in reader]
-
-    # Execute the SQL query for each batch of data in the list
-    n = 3 # batch-size
-    for j in range(0, len(data), n):
-        batch = data[j:j+n]
-        cursor.executemany(sql, batch)
-        mydb.commit()
-
-    # Add a one-minute delay before the next iteration
-    time.sleep(60)
+        data = [(row[0], row[1], row[2], row[-1],int(row[-7]),int(time.time() * 1000)) for row in reader]
+    cursor.executemany(sql, data)
+    mydb.commit()
+    # Add a one-second delay before the next iteration
+    time.sleep(1)
 
 # Close the database connection
 mydb.close()
