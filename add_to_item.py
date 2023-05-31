1ce3d09f1b94cd784f6086e5949fd7a0aee34c7c
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-05-31 14:00:26 +0530

    Impelement adding data to order and item tables

--- add_to_item.py
+++ add_to_item.py
@@ -8,14 +8,14 @@ import configparser
 config = configparser.ConfigParser()
 
 # Read the properties file
-config.read('/home/nuvidu/fyp/debezium/add_to_mysql/app.properties')
+config.read('join.properties')
 
 # Get the values
 host = config.get('database', 'host')
 database = config.get('database','database')
 port = config.get('database', 'port')
 user = config.get('database', 'user')
-data_dir = config.get('data','initial_dir')
+data_dir = config.get('data','dir')
 password = config.get('database', 'password')
 
 # Establish a connection to the MySQL database
@@ -30,21 +30,17 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 # Define the SQL query to insert data into the table
-sql = "INSERT INTO networkTraffic (ip, date, timestamp, browser, traffic, eventTimestamp) VALUES (%s, %s, %s, %s, %s, ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000))"
-
-n=1
-
+sql = "INSERT INTO item (itemType, unitPrice, unitCost) VALUES (%s, %s, %s)"
 
 # Run the loop n times
 while True:
   
-  browsers = ['chr', 'fox', 'mie', 'opr', 'saf']
   # Read data from the CSV file
   with open(data_dir, 'r') as file:
       reader = csv.reader(file)
       next(reader) # Skip the header row
       for row in reader:
-        data = (row[0], row[1], row[2], browsers[random.randint(0,4)],float(row[-7]))
+        data = (row[2], row[9], row[10])
         cursor.execute(sql, data)
         print(sql)
         mydb.commit()
--- add_to_order.py
+++ add_to_order.py
@@ -8,14 +8,14 @@ import configparser
 config = configparser.ConfigParser()
 
 # Read the properties file
-config.read('/home/nuvidu/fyp/debezium/add_to_mysql/app.properties')
+config.read('join.properties')
 
 # Get the values
 host = config.get('database', 'host')
 database = config.get('database','database')
 port = config.get('database', 'port')
 user = config.get('database', 'user')
-data_dir = config.get('data','initial_dir')
+data_dir = config.get('data','dir')
 password = config.get('database', 'password')
 
 # Establish a connection to the MySQL database
@@ -30,21 +30,17 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 # Define the SQL query to insert data into the table
-sql = "INSERT INTO networkTraffic (ip, date, timestamp, browser, traffic, eventTimestamp) VALUES (%s, %s, %s, %s, %s, ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000))"
-
-n=1
-
+sql = "INSERT INTO order(orderID, itemType, orderPriority, orderDate, shipDate, unitsSold , totalRevenue, totalCost, totalProfit, region , country , eventTimestamp) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000))"
 
 # Run the loop n times
 while True:
   
-  browsers = ['chr', 'fox', 'mie', 'opr', 'saf']
   # Read data from the CSV file
   with open(data_dir, 'r') as file:
       reader = csv.reader(file)
       next(reader) # Skip the header row
       for row in reader:
-        data = (row[0], row[1], row[2], browsers[random.randint(0,4)],float(row[-7]))
+        data = (row[6], row[2], row[4], row[5],row[7],row[8],row[11],row[12],row[13],row[0],row[1])
         cursor.execute(sql, data)
         print(sql)
         mydb.commit()
